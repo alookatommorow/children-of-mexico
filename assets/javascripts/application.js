8b5044838862554950c774aaa3ea8dba001d4ccd
alookatommorow <hessjohnt@gmail.com>
2016-09-26 22:53:02 -0700

    add styling to dots

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -14,76 +14,7 @@ $(document).ready(function() {
 
   if (window.location.pathname === "/") {
     new ImageRotator().init();
-
-
   }
-
-
-  // var currentIndex = 0,
-  //     $photos = $('.photo-container img'),
-  //     lastIndex = $photos.length - 1,
-  //     $thumbs = $(".circle");
-
-  // $photos.eq(currentIndex).show();
-  // $thumbs.first().children().addClass("active-photo");
-
-  // function selectNextThumb(next) {
-  //   $(".active-photo").removeClass("active-photo");
-  //   next.addClass("active-photo");
-  // }
-
-  // function showNext() {
-  //   var $currentPhoto = $photos.eq(currentIndex);
-  //   var $nextThumb = $thumbs.eq(currentIndex + 1).children();
-  //   if (currentIndex === lastIndex) {
-  //     $currentPhoto.hide();
-  //     $photos.first().show();
-  //     selectNextThumb($thumbs.first().children());
-  //     currentIndex = 0;
-  //   } else {
-  //     $currentPhoto.hide();
-  //     $currentPhoto.next().show();
-  //     selectNextThumb($nextThumb);
-  //     currentIndex++;
-  //   }
-  // }
-
-  // function showPrev() {
-  //   var $currentPhoto = $photos.eq(currentIndex),
-  //       $nextThumb = $thumbs.eq(currentIndex - 1).children();
-  //   if (currentIndex === 0) {
-  //     $currentPhoto.hide();
-  //     $photos.last().show();
-  //     selectNextThumb($nextThumb);
-  //     currentIndex = lastIndex;
-  //   } else {
-  //     $currentPhoto.hide();
-  //     $currentPhoto.prev().show();
-  //     selectNextThumb($nextThumb);
-  //     currentIndex--;
-  //   }
-  // }
-
-  // $(".carousel-button").click(function(){
-  //   if ($(this).data("carousel") === "prev") {
-  //     showPrev();
-  //   } else {
-  //     showNext();
-  //   }
-  // });
-
-  // $('.skatepark-show-image').click(function() {
-  //   showNext();
-  // });
-
-  // $('.skatepark-show-thumbnail').click(function() {
-  //   nextIndex = $(this).index();
-  //   var $currentPhoto = $photos.eq(currentIndex);
-  //   $currentPhoto.hide();
-  //   $photos.eq(nextIndex).show();
-  //   selectNextThumb($thumbs.eq(nextIndex).children());
-  //   currentIndex = nextIndex;
-  // });
 });
 
 function ImageRotator() {
@@ -106,11 +37,11 @@ function ImageRotator() {
       $nextImage = $("img.first");
       $nextCircle = $(".circle.first");
     }
-    $currentImage.fadeOut(1500, function() {
+    $currentImage.fadeOut(1000, function() {
       $currentImage.removeClass("active");
-      $nextImage.addClass("active").fadeIn(1500);
+      $nextImage.addClass("active").fadeIn(1000);
       $currentCircle.removeClass("active");
-      $nextCircle.addClass("active").fadeIn(1500);
+      $nextCircle.addClass("active");
     });
   }
 
@@ -123,9 +54,9 @@ function ImageRotator() {
     $("img").removeClass("active");
     corrImage.addClass("active");
     $(this).addClass("active");
-    $("img:not(.active)").css("display", "none");
+    $(".photo-container img:not(.active)").css("display", "none");
     corrImage.css("display", "block");
-    interval = setInterval(animate, 4000);
+    interval = setInterval(animate, 5000);
   });
 
 
